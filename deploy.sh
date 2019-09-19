ac16a214b8a32bbcc7ad7351877df50f42eccc5d
黄星宾 <356578436@qq.com>
2019-09-19 17:25:20 +0800

    修改travis设置

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:HuangXingBin/HuangXingBin.github.io.git master:gh-pages
+git push -f git@github.com:HuangXingBin/HuangXingBin.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# git push -f git@github.com:<USERNAME>/<REPO>.git master
 
 cd -
