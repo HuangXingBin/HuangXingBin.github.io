8971d3868f4ba4faa5cc13645c9990ba338252d6
黄星宾 <356578436@qq.com>
2019-09-19 21:56:46 +0800

    test1

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:HuangXingBin/HuangXingBin.github.io.git master
+git push -f https://${GITHUB_TOKEN}@github.com:HuangXingBin/HuangXingBin.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master
